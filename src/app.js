c9e4a898d9f8abd7c49fc3173bb43a498abda1c5
Siddant <siddantgurung@hotmail.co.uk>
2019-09-08 21:37:31 +0100

    testing complete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import posts from '../data/posts';
 
-import { connect } from 'react-redux';
-import { selectUsers, selectUsersId } from './selectors/index'
 import './main.scss';
 import Image from './components/Image';
 
@@ -10,36 +8,17 @@ class App extends React.Component {
 	constructor() {
 		super();
 	}
-	componentDidMount() {
-		console.log(this.props.selectUsersId)
 
-
-	}
 	render() {
 		return (
 			<main>
 				<h1>Images</h1>
 				<div className="images">
-					{posts.map((elem, index) => <Image key={index} imgSrc={elem.display_src} caption={elem.caption} />)}
+					{posts.map((elem, index) => <Image key={index} code={elem.code} />)}
 				</div>
 			</main>
 		);
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {
-		comments: state.comments,
-		posts: state.posts,
-		selectUsers: selectUsers(state),
-		selectUsersId: selectUsersId(state),
-	}
-}
-
-
-// const mapDispatchToProps = () => {
-
-// };
-
-export default connect(mapStateToProps, null)(App);
-// export default App;
+export default App;
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
-const Image = ({ imgSrc, caption }) => {
-	return (
-		<div className="imageContainer">
-			<img src={imgSrc} />
-			<p>{caption}</p>
-		</div>
-	);
-};
+import { connect } from 'react-redux';
+import {
+	selectedId,
+	selectPostId,
+	selectedPost
 
-export default Image;
+} from '../selectors/index'
+
+
+class Image extends React.Component {
+	constructor() {
+		super();
+	}
+	render() {
+		const { display_src, caption } = this.props.selectPostId.posts
+		const { comments } = this.props.selectPostId
+		return (
+			<div className="imageContainer">
+				<img src={display_src} />
+				<p>{caption}</p>
+				<p>{(comments || []).length} comments</p>
+			</div>
+		);
+	}
+}
+
+const makeMapStateToProps = () => {
+	const mapStateToProps = (state, props) => {
+		return {
+			// selectedPost: selectedPost(state, props),
+			selectPostId: selectPostId(state, props),
+			selectedId: selectedId(state, props),
+		}
+	}
+	return mapStateToProps
+}
+
+export default connect(makeMapStateToProps, null)(Image);
